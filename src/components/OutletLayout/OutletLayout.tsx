0ceb51ca6a90a70b9f6a85d8cf6918a8cb353c7e
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-08-02 16:39:21 +0500

    changes in holiday modal

--- src/components/OutletLayout/OutletLayout.tsx
+++ src/components/OutletLayout/OutletLayout.tsx
@@ -5,7 +5,7 @@ export interface OutletLayoutProps{
 }
 const OutletLayout=({children})=>{
     return <div className="w-full p-4 bg-whiteColor shadow-smShadow bg-whitecolor m-auto border-[1px] border-borderColor border-solid rounded-xl 
-                            sm:p-6  sm:w-[95%] xl:w-[100%]">
+                            sm:p-6  sm:w-[95%] xl:w-[100%] h-[80vh]">
         
         {children}
         
