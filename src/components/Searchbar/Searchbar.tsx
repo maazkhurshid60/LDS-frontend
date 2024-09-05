817cb286bf08ece12d69157c99b244a5ef384a32
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-09-05 14:58:30 +0500

    latest code

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -8,9 +8,9 @@ export interface SearchbarProps{
 
 
  const Searchbar:React.FC<SearchbarProps>=({value,onChange})=>{
-    return<div className=" border-[1px] border-borderColor border-solid rounded-md px-2 py-1 sm:w-[200px] text-grayColor flex items-center gap-2">
+    return<div className=" border-[1px] border-borderColor border-solid rounded-md px-2 py-1  sm:w-[200px] text-grayColor flex items-center gap-2">
         <FiSearch/>
-        <input className="w-[80%] focus:outline-none" placeholder="Search..." value={value} onChange={onChange}/>
+        <input className="w-[80%] focus:outline-none bg-whiteColor" placeholder="Search..." value={value} onChange={onChange}/>
         </div>
 }
 
