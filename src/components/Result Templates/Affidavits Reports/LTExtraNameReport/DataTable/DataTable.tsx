96ff325bb53f2c5e09b8db1c01233a12f8427742
GESI LDS <gesilds1@gmail.com>
2024-11-01 15:54:07 +0500

    work on lt extra names

--- src/components/Result Templates/Affidavits Reports/LTExtraNameReport/DataTable/DataTable.tsx
+++ src/components/Result Templates/Affidavits Reports/LTExtraNameReport/DataTable/DataTable.tsx
@@ -15,78 +15,62 @@ const DataTable: React.FC<DataTableProps> = (item) => {
         const extraNames = item?.serviceResultlTServed?.split(",") || [];
         return total + extraNames?.length;
     }, 0);
-    return < div className="relative overflow-x-auto" >
-        <table className="w-full text-sm border-[2px] border-solid border-blackColor ">
-            <thead className="text-xs  border-[2px] border-solid border-blackColor">
+    return < div className="relative overflow-x-auto mt-4" >
+        <table className="w-full text-sm border-[2px] border-solid border-blackColor">
+            {/* Move the header outside the table */}
+            <thead className="text-xs border-[2px] border-solid border-blackColor">
                 <tr>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        Case#/DOM
+                    <th colSpan={10} className="text-center px-1 py-1 border-[1px] border-solid border-blackColor">
+                        <div className="w-[500px] mx-auto  flex justify-center print-header">
+                            <h1 className="font-bold flex items-center gap-x-2 text-xl uppercase">L&T Extra Names Report</h1>
+                        </div>
                     </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        Case Paper
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        Index No
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        Adress
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        APT
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        LT Names
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        LT Extra Names
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        Add
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        Xtr
-                    </th>
-                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">
-                        RRR
-                    </th>
+                </tr>
+                <tr>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">Case#/DOM</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">Case Paper</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">Index No</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">Address</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">APT</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">LT Names</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">LT Extra Names</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">Add</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">Xtr</th>
+                    <th scope="col" className="px-1 py-1 text-xs border-[1px] border-solid border-blackColor">RRR</th>
                 </tr>
             </thead>
             <tbody>
-                {item?.data?.map(data => <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-[2px] border-solid border-blackColor">
-                    <th scope="row" className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor text-gray-900 whitespace-nowrap dark:text-white">
-                        {data?.caseNo ? data?.caseNo : "________"}
-                    </th>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        Dispo/HO
-                    </td>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        {data?.oLTIndexNo ? data?.oLTIndexNo : "________"}
-                    </td>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        {data?.lTSAddress ? data?.lTSAddress : "________"}                        </td>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        {data?.lTSApt ? data?.lTSApt : "________"}
-                    </td>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        {data?.serviceResultlTServed ? data?.serviceResultlTServed : "________"}
-                    </td>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        {data?.serviceResultlTNotServed ? data?.serviceResultlTNotServed : "________"}
-                    </td>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        -
-                    </td>
-                    <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        {data?.serviceResultlTNotServed?.split(",")?.length > 0 ? data?.serviceResultlTNotServed?.split(",")?.length : "________"}
-                    </td><td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
-                        -
-                    </td>
-
-                </tr>
-                )}
-
+                {item?.data?.map(data => (
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-[2px] border-solid border-blackColor" key={data.caseNo}>
+                        <th scope="row" className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor text-gray-900 whitespace-nowrap dark:text-white">
+                            {data?.caseNo ? data?.caseNo : "________"}
+                        </th>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">Dispo/HO</td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
+                            {data?.oLTIndexNo ? data?.oLTIndexNo : "________"}
+                        </td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
+                            {data?.lTSAddress ? data?.lTSAddress : "________"}
+                        </td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
+                            {data?.lTSApt ? data?.lTSApt : "________"}
+                        </td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
+                            {data?.serviceResultlTServed ? data?.serviceResultlTServed : "________"}
+                        </td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
+                            {data?.serviceResultlTNotServed ? data?.serviceResultlTNotServed : "________"}
+                        </td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">-</td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">
+                            {data?.serviceResultlTNotServed?.split(",")?.length > 0 ? data?.serviceResultlTNotServed?.split(",")?.length : "________"}
+                        </td>
+                        <td className="px-3 py-3 text-xs border-[1px] border-solid border-blackColor">-</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
+
         {/* SUMMARY STARTS */}
         <div className="flex items-center justify-between gap-x-8 font-bold text-base mt-8">
             <h1>Test Cases: {item?.testCaseLength}</h1>
--- src/components/Result Templates/Affidavits Reports/LTExtraNameReport/MainTemplate.tsx
+++ src/components/Result Templates/Affidavits Reports/LTExtraNameReport/MainTemplate.tsx
@@ -1,72 +1,82 @@
-import React, { forwardRef, useEffect, useRef, useState } from "react";
-import TemplateOutlet from "../../TemplateOutlet";
-import DataTable from "./DataTable/DataTable";
-import ReactToPrint from "react-to-print";
-import Button from "../../../Buttons/Button/Button";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../../redux/store";
-export interface LiNonReportsProps {
-    props?: any;
-}
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import ReactToPrint from 'react-to-print';
+import TemplateOutlet from "../../TemplateOutlet"
+import Button from "../../../Buttons/Button/Button"
+import { RootState } from "../../../../redux/store"
+import DataTable from './DataTable/DataTable';
 const LTExtraNameReport = () => {
-    const ltExtraNameReportPrintRef = useRef<HTMLButtonElement | null>(null);
-    const legalDeliveryDataa = useSelector((state: RootState) => state?.legalDelivery.selectedLegalDeliveryData)
+    const ltExtraNameReportPrintRef = useRef<HTMLDivElement | null>(null);
+    const legalDeliveryData = useSelector((state: RootState) => state.legalDelivery.selectedLegalDeliveryData);
     const [resultData, setResultData] = useState([]);
-    const [extraltName, setExtraltName] = useState([]);
 
-    const [dataTable, setDataTable] = useState({
-        caseNo: "",
-        index: "",
-        address: "",
-        apt: "",
-        ltnames: "",
-        extraname: []
-
-    })
     useEffect(() => {
+        setResultData(legalDeliveryData?.data || []);
+    }, [legalDeliveryData]);
 
-        setResultData(legalDeliveryDataa?.data)
-
-
-    }, [])
-
-    console.log(resultData?.map(data => data?.caseNo))
-    return <>
-        <div className="absolute h-[83.5vh] overflow-y-scroll relative">
-            <TemplateOutlet>
-
-                <div className="w-full flex justify-center">
-                    <h1 className="font-bold flex items-center gap-x-2 text-xl uppercase  ">L&T Extra Names Report</h1>
-                </div>
-                <DataTable data={resultData} testCaseLength={resultData?.length} />
-            </TemplateOutlet>
-
-
-
-            <div className="flex justify-end mt-5 mb-5 mr-5">
-                <ReactToPrint
-                    trigger={() => (
-                        <div className="w-[10%]">
-                            <Button text="Print" />
-                        </div>
-                    )}
-                    content={() => ltExtraNameReportPrintRef.current}
-                />
-            </div>
-        </div>
-        <div style={{ display: "none" }}>
-            {/* The content to print */}
-            <div ref={ltExtraNameReportPrintRef} className="mt-2">
+    return (
+        <>
+            <div className="absolute h-[83.5vh] overflow-y-scroll relative">
                 <TemplateOutlet>
 
-                    <div className="w-full flex justify-center ">
-                        <h1 className="font-bold flex items-center gap-x-2 text-xl uppercase  ">L&T Extra Names Report</h1>
-                    </div>
-                    <DataTable data={resultData} testCaseLength={resultData?.length} />
+                    <DataTable data={resultData} testCaseLength={resultData.length} />
                 </TemplateOutlet>
-            </div>
-        </div>
-    </>
-}
 
-export default LTExtraNameReport
+                <div className="flex justify-end mt-5 mb-5 mr-5">
+                    <ReactToPrint
+                        trigger={() => (
+                            <div className="w-[10%]">
+                                <Button text="Print" />
+                            </div>
+                        )}
+                        content={() => ltExtraNameReportPrintRef.current}
+                    />
+                </div>
+            </div>
+
+            {/* Hidden print container */}
+            <div style={{ display: "none" }}>
+                <div ref={ltExtraNameReportPrintRef} className="mt-2 print-container">
+                    {/* Print each page with the header */}
+                    {Array.from({ length: Math.ceil(resultData.length / 20) }).map((_, pageIndex) => (
+                        <div key={pageIndex} className="page">
+
+                            <DataTable data={resultData.slice(pageIndex * 20, (pageIndex + 1) * 20)} testCaseLength={resultData.length} />
+                            {/* Page break */}
+                            <div style={{ pageBreakAfter: 'always' }} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <style>{`
+                @media print {
+                    .print-container {
+                        page-break-inside: avoid;
+                    }
+                    
+                    .print-header {
+                        display: block;
+                        font-weight: bold;
+                        text-align: center;
+                        font-size: 18px;
+                        padding: 10px 0;
+                        border-bottom: 1px solid #000;
+                        margin-bottom: 10px;
+                    }
+
+                    .page {
+                        page-break-after: always;
+                    }
+
+                    .DataTable-row {
+                        page-break-inside: avoid;
+                        page-break-after: auto;
+                    }
+                }
+            `}</style>
+        </>
+    );
+};
+
+export default LTExtraNameReport;
