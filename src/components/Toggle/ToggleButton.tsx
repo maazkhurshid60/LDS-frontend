7066f54b6d6a990185d09cd0c87ae1a140c780cd
GESI-LDS <gesilds1@gmail.com>
2024-11-12 20:12:49 +0500

    latest code

--- src/components/Toggle/ToggleButton.tsx
+++ src/components/Toggle/ToggleButton.tsx
@@ -8,9 +8,9 @@ interface ToggleButtonProps {
 
 const ToggleButton: React.FC<ToggleButtonProps> = ({ label, value, onChange }) => {
     return (
-        <div>
-            <label className="inline-flex items-center cursor-pointer flex items-center w-full justify-between">
-                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 ">{label}</span>
+        <div className='m-auto'>
+            <label className="inline-flex items-center cursor-pointer flex items-center w-full gap-x-3 ">
+                <span className="capitalize font-medium text-sm md:text-lg opacity-[65%]">{label}</span>
                 <input
                     type="checkbox"
                     checked={value} // Bind the checked state to `value`
@@ -22,7 +22,7 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({ label, value, onChange }) =
                 rtl:peer-checked:after:-translate-x-full peer-checked:after:border-accentBlue peer-checked:after:border-[2px] 
                 peer-checked:after:border-solid after:content-[''] after:absolute after:top-[0px] after:start-[4px] 
                 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 
-                after:transition-all after:duration-[1000ms] dark:border-accentBlue peer-checked:bg-blue-600">
+                after:transition-all after:duration-[500ms] dark:border-accentBlue peer-checked:bg-blue-600">
                 </div>
 
                 {/* <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div> */}
--- src/pages/NotFound/ServerDownPage.tsx
+++ src/pages/NotFound/ServerDownPage.tsx
@@ -3,6 +3,7 @@ import ToggleButton from '../../components/Toggle/ToggleButton'
 import axios from 'axios'
 import { baseUrl } from '../../apiservices/baseUrl/baseUrl'
 import { toast } from 'react-toastify'
+import Button from '../../components/Buttons/Button/Button'
 
 const ServerDownPage = () => {
     const [isServerDown, setIsServerDown] = useState(false)
@@ -19,16 +20,40 @@ const ServerDownPage = () => {
     }
 
 
-    return (
-        <>
-            <ToggleButton
-                label={"Down the Server"}
-                value={isServerDown}
-                onChange={() => setIsServerDown(!isServerDown)}
-            />
+    return (<div className="w-full bg-whiteColor flex items-center justify-center h-[100vh] ">
+        <div className="w-[95%] border-[1px] border-borderColor border-solid rounded-xl text-center shadow-lgShadow py-6 px-8
+                          sm:w-[65%] 
+                          md:py-8 md:px-10 
+                           lg:py-10 lg:px-12
+                          xl:w-[35%] xl:py-12 xl:px-14">
+            {/* HEADER STARTS */}
+            <div className="flex flex-col items-center justify-center gap-4 mb-4">
+                <h1 className="px-6 tracking-wide py-2 bg-primaryColor rounded-md 
+                             inline-block text-whiteColor font-semibold text-base
+                             sm:text-xl
+                             lg:text-2xl ">
+                    LDS
+                </h1>
+                <p className="capitalize font-medium text-sm md:text-lg opacity-[65%]">Down the Server</p>
+            </div>
+            {/* HEADER ENDS */}
+            {/* FORM STARTS */}
+            <div className='m-auto'>
 
-            <button onClick={serverDownFunction}>Save</button>
-        </>
+                <ToggleButton
+                    label={"Down the Server"}
+                    value={isServerDown}
+                    onChange={() => setIsServerDown(!isServerDown)}
+                />
+            </div>
+
+            <div className='w-[40%] m-auto mt-4'>
+
+                <Button text='Save' onClick={serverDownFunction} />
+            </div>
+            {/* FORM ENDS */}
+        </div>
+    </div>
     )
 }
 
