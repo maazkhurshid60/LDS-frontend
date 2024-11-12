b9405f944d7b91cddf21caf8300af69678cf4882
GESI-LDS <gesilds1@gmail.com>
2024-11-12 19:32:32 +0500

    latest code

--- src/pages/NotFound/ServerDownPage.tsx
+++ src/pages/NotFound/ServerDownPage.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react'
 import ToggleButton from '../../components/Toggle/ToggleButton'
+import axios from 'axios'
+import { baseUrl } from '../../apiservices/baseUrl/baseUrl'
 
 const ServerDownPage = () => {
     const [isServerDown, setIsServerDown] = useState(false)
-    const serverDownFunction = () => {
-        alert(isServerDown)
+    const serverDownFunction = async () => {
+        const data = { status: isServerDown }
+        try {
+            const response = await axios.post(`${baseUrl}/server/control`, data)
+            console.log("response>>>>>>>>>>>>>>>>>>>", response)
+        } catch (error) {
+            console.log("error>>>>>>>>>>>>>>>>>>>>>>", error)
+        }
     }
+
+
     return (
         <>
             <ToggleButton
