528d53f9fa27bcb1152be840df656e523b5d1a09
GESI-LDS <gesilds1@gmail.com>
2024-11-12 19:58:50 +0500

    latest code

--- src/pages/NotFound/ServerDownPage.tsx
+++ src/pages/NotFound/ServerDownPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ToggleButton from '../../components/Toggle/ToggleButton'
 import axios from 'axios'
 import { baseUrl } from '../../apiservices/baseUrl/baseUrl'
+import { toast } from 'react-toastify'
 
 const ServerDownPage = () => {
     const [isServerDown, setIsServerDown] = useState(false)
@@ -11,6 +12,7 @@ const ServerDownPage = () => {
         try {
             const response = await axios.post(`${baseUrl}/server/control`, data)
             console.log("response>>>>>>>>>>>>>>>>>>>", response)
+            toast?.success(`${response?.data}`)
         } catch (error) {
             console.log("error>>>>>>>>>>>>>>>>>>>>>>", error)
         }
